3bc4ed0cab60482e5e8f03ec641d39afc8b88e4f
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:14:20 -0400

    Ejercicio 2

---
+++ Ejercicio2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] valor = new float[5];
+
+            for (int f = 0; f < 5; f++)
+            {
+                Console.WriteLine("Ingrese numero verdadero: ");
+                valor[f] = float.Parse(Console.ReadLine());
+            }
+            for (int f = 4; f >= 0; f--)
+            {
+                Console.WriteLine("Valor: " + valor[f]);
+            }
+
+            }
+        }
+}
