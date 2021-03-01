2168af5a59537de87b42bcb2b71d815ccf2edc6a
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:52:16 -0400

    Add files via upload

---
+++ Ejercicio7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicios7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+            foreach (char letras in nombre)
+            {
+                Console.WriteLine(letras);
+            }
+
+            }
+        }
+}
