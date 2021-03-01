121d7e9a09e648ec50cf0d52e2b404ce062418ab
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:52:59 -0400

    Add files via upload

---
+++ Ejercicio9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicios9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, multi = 1, j = 0; ;
+            do
+            {
+
+                Console.Write("Ingresar valores: ");
+                valor = int.Parse(Console.ReadLine());
+                for (int i = 0; i < 11; i++)
+                {
+                    multi = i * valor;
+                    Console.WriteLine("{0} x {1} = {2}", valor, i, multi);
+                }
+
+            } while (valor != -1);
+            Console.ReadKey();
+        }
+        public static void Main(string[] args)
+        {
+            MainClass p = new MainClass();
+            p.cargar();
+
+        }
+    }
+}
