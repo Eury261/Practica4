5bafb52eb2dbc7316c330664b1ef7c921fb0c4d8
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:52:37 -0400

    Add files via upload

---
+++ Ejercicio8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicios8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] altura = new float[5];
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write("Ingrese altura: ");
+                altura[i] = float.Parse(Console.ReadLine());
+            }
+
+            float suma = 0, pro;
+            for (int i = 0; i < 5; i++)
+            {
+                suma += altura[i];
+            }
+            pro = suma / 5;
+            Console.WriteLine("Promedio: " + pro);
+
+            int mayores = 0, menores = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (altura[i] > pro)
+                {
+                    mayores++;
+                }
+                if (altura[i] < pro)
+                {
+                    menores++;
+                }
+            }
+            Console.WriteLine("Personas mayores: " + mayores);
+            Console.WriteLine("Personas menores: " + menores);
+
+        }
+    }
+}
