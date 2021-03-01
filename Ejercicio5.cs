8739c92b1679a92c2dc4068467b33fb88fe13dd4
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:51:52 -0400

    Add files via upload

---
+++ Ejercicio5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicios5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.Write("Ingrese nombre: ");
+                nombre[i] = Console.ReadLine();
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, nombre[i]);
+
+            }
+        }
+}
