54370056c2b10e474695c58191ebb7b98c3b06ab
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:53:21 -0400

    Add files via upload

---
+++ Ejercicio10.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ejercicios10
+{
+    class MainClass
+    {
+        private string nombre;
+        private int edad;
+
+        public void cargar()
+        {
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingrese una edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+        }
+
+        public void mensaje()
+        {
+            if (edad >= 18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+            Console.ReadKey();
+        }
+
+        public static void Main(string[] args)
+        {
+            MainClass p = new MainClass();
+            p.cargar();
+            p.mostrar();
+            p.mensaje();
+
+        }
+    }
+}
