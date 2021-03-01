228518209fd1f44fdd4dce14cf1707ac6e8f541d
eury <78828180+Eury261@users.noreply.github.com>
2021-02-28 23:51:29 -0400

    Add files via upload

---
+++ Ejercicio4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicios4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int i, mayor = 0, n = 0;
+
+            for (i = 0; i < 10; i++)
+            {
+
+                Console.Write("Ingrese un numero: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            while (n < 10)
+            {
+                if (numeros[n] > mayor)
+                {
+                    mayor = numeros[n];
+                }
+                n++;
+            }
+            Console.WriteLine("El mayor es el " + mayor);
+            Console.ReadKey();
+
+        }
+    }
+}
